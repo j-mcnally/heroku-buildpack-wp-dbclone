657b83d1fbfd17beef39ca0885eaed47665311bc
Justin McNally <justin@kohactive.com>
2016-01-12 11:36:57 -0600

    initial commit

---
+++ deps/db.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+APP_URL=`cat appurl.txt`
+
+PATH=$PATH:$PWD/vendor/node/bin:$PWD/.wpcli:$PWD/.heroku/vendor/mysql/bin
+LD_LIBRARY_PATH=$LIBRARY_PATH:$PWD/.heroku/vendor/mysql/lib
+LIBRARY_PATH=$LIBRARY_PATH:$PWD/.heroku/vendor/mysql/lib
+
+ORIG_JAWSDB_URL=$JAWSDB_URL
+JAWSDB_URL=$STAGING_DB_URL wp db export staging.bk.sql
+JAWSDB_URL=$ORIG_JAWSDB_URL
+wp db import staging.bk.sql
+
+
+echo "SETTING URL TO: $APP_URL"
+
+wp option update home $APP_URL
+wp option update siteurl $APP_URL
